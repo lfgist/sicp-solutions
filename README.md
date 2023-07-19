cf8c3f2d90d829835b15b56fd48ae248c9548e58
LFGist <87042183+lfgist@users.noreply.github.com>
2023-07-19 13:00:08 +0200

    solutions <= 1.8

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 ## Notes
 
 * I am following SICP, 2nd ed.
-* I am using the [SICP Collections](https://docs.racket-lang.org/sicp-manual/).
+* I am using DrRacket with the [SICP Collections](https://docs.racket-lang.org/sicp-manual/). On some machines, I use VSC with the Magic Racket extension.
+
+## Flow State
+
+* Music:
+  - [Music for Programming](https://musicforprogramming.net/)
+  - Spotify Playlists:
+    - [Flow State](https://open.spotify.com/playlist/4qm2j9owvCxoQ690FdJoQn?si=c8df4b9c9fe2496a) by Renan Zukovski Fernandes
--- chapter-01.rkt
+++ chapter-01.rkt
@@ -42,3 +42,50 @@
 ;;; Exercise 1.4
 
 ; Returns a + |b|.
+
+;;; Exercise 1.5
+
+; When the function p evaluates, it calls itself, which leads to an infinite loop.
+; An applicative-order interpreter will evaluate all the arguments, including p, which leads to an infinite loop.
+; A normal-order interpreter will evaluate the argument values as needed, and (test 0) will resolve when when (= x 0) returns #t.
+
+;;; Exercise 1.6
+
+; When the function new-if is called, it evaluates all its arguments, and so sqrt-iter keeps calling itself
+; which leads to an infinite loop.
+
+;;; Exercise 1.7
+
+(define (percent-change old-value new-value)
+      (/ (- new-value old-value) old-value))
+
+(define (good-enough? old-guess new-guess)
+      (< (abs (percent-change old-guess new-guess)) 0.00001))
+
+(define (average x y)
+      (/ (+ x y) 2))
+
+(define (improve guess x)
+      (average guess (/ x guess)))
+
+(define (sqrt-iter-improved old-guess new-guess x)
+      (if (good-enough? old-guess new-guess)
+      new-guess
+      (sqrt-iter-improved new-guess (improve new-guess x) x)))
+
+(define (sqrt x)
+      (sqrt-iter-improved x 1.0 x))
+
+;;; Exercise 1.8
+
+(define (cbrt-improve guess x)
+      (/ (+ (/ x (* guess guess)) (* 2 guess))
+      3))
+
+(define (cbrt-iter-improved old-guess new-guess x)
+      (if (good-enough? old-guess new-guess)
+      new-guess
+      (cbrt-iter-improved new-guess (cbrt-improve new-guess x) x)))
+
+(define (cbrt x)
+      (cbrt-iter-improved x 1.0 x))
